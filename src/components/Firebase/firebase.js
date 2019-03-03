73951dff06811ea4f9d6cc3eb9bce79e79902ae3
adamkiryk <adamkiryk@gmail.com>
2019-03-03 07:29:26 -0500

    Create routes that go to dummy pages

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,8 @@
+const config = {
+  apiKey: "AIzaSyD5W3OyAXjPnx40Cz3Yflaf14-xddY4sKA",
+  authDomain: "storyengine-9081e.firebaseapp.com",
+  databaseURL: "https://storyengine-9081e.firebaseio.com",
+  projectId: "storyengine-9081e",
+  storageBucket: "storyengine-9081e.appspot.com",
+  messagingSenderId: "739360779225"
+};
