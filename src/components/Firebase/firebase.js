4195b4f1468457b3ca9c429cd48be927e68eb417
adamkiryk <adamkiryk@gmail.com>
2019-03-10 17:00:11 -0400

    Update fb

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,5 +1,6 @@
 import app from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/database'; // import the realtime database
 
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -14,6 +15,7 @@ class Firebase {
   constructor() {
     app.initializeApp(config);
     this.auth = app.auth();
+    this.db = app.database(); // initialize the realtime database
   }
 
   // *** Auth API ***
@@ -30,6 +32,12 @@ class Firebase {
 
   doPasswordUpdate = password =>
     this.auth.currentUser.updatePassword(password);
+
+  // *** User API ***
+
+  user = uid => this.db.ref(`users/${uid}`); // The paths in the ref() method match the location where your entities (users) will be stored in Firebase’s realtime database API
+
+  users = () => this.db.ref('users');
 }
 
 export default Firebase;
